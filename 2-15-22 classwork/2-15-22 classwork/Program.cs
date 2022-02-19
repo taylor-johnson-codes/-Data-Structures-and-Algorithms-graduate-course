27ca5d48915f0a8ba0b3f6153b16ba49a6c7568c
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-18 17:49:17 -0800

    added code and comments

--- 2-15-22 classwork/2-15-22 classwork/Program.cs
+++ 2-15-22 classwork/2-15-22 classwork/Program.cs
@@ -13,10 +13,12 @@ namespace _2_15_22_classwork
             // add code to ask user for size (skipped)
             //int[] values = new int [size];
             
-            //int[] values = new int [10];  // 0s are in the array by default in C#
+            //int[] values = new int [10];  // 0s are in the array by default in C# (but they're not accessible)
             // can't add an 11th element onto the standard array
 
+
             // ArrayList allows for the array size to grow and shrink
+            // Built-in ArrayList and built-in .Add(): 
 
             ArrayList myArrList = new ArrayList();  // myArrList points to new ArrayList object
 
@@ -25,7 +27,7 @@ namespace _2_15_22_classwork
             myArrList.Add(10);
             Console.WriteLine($"Current size: {myArrList.Count}, capacity: {myArrList.Capacity}");  // 1, 4
             // allocates memory in a chunk of 4 element spots to start, then doubles the current capacity each time it needs more space
-            // empty spots contain 0s but they're not accessible
+            // empty spots contain 0s, but they're not accessible
 
             myArrList.Add(20);
             myArrList.Add(30);
@@ -41,34 +43,95 @@ namespace _2_15_22_classwork
             */
 
 
-            // to create my own data type/structure, create a new class
-            MyArrayList myList = new MyArrayList(2);
-            Console.WriteLine($"Current size: {myList.Size}, capacity: {myList.Capacity}");
+            // Now using our own methods and properties from scratch (not built-ins):
+            // To create my own data type/data structure, create a new class
 
-            // see his code for how do do foreach loop on the class I created; can't do by default; have to add : I... to class and another method
-            // yield return: to keep returning in a loop for each iteration
+            MyArrayList<string> myList = new MyArrayList<string>(2);  // myList points to a new MyArrayList object with a capacity of two elements; it's a generic/type-safe class so you have to specify data type with <>
+            //myList.Size = 5;  // doesn't work, .Size is a read-only property outside of its class
 
-            //MyArrayList<string> testList = new MyArrayList<string>;  // says to replace T in class with string
-            //MyArrayList<int> testList2 = new MyArrayList<int>;  // says to replace T in class with string
+            Console.WriteLine("STRING ARRAY");
+            Console.WriteLine($"Array is empty: {myList.IsEmpty()}");
 
+            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");  // .Size and .Capacity properties were created in the MyArrayList class below
+
+            myList.Add("Saint");
+            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+
+            myList.Add("Martin's");
+            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+
+            myList.Add("University");
+            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+
+
+            Console.WriteLine($"Array is empty: {myList.IsEmpty()}");
+
+            // print the values (foreach loop doesn't work without IEnumerable/GetEnumerator in MyArrayList class)
+            Console.Write("Array values:  ");
+            foreach (var val in myList)
+                Console.Write($"{val}   ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            MyArrayList<int> numbers = new MyArrayList<int>();  // will use default capacity of 4
+            Console.WriteLine("INTEGER ARRAY");
+            Console.WriteLine($"Array is empty: {numbers.IsEmpty()}");
+
+            numbers.Add(30);
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            numbers.AddFirst(10);
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            numbers.Insert(20, 1);
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            numbers.Add(40);
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            numbers.AddLast(50);
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+
+            Console.WriteLine($"Array is empty: {numbers.IsEmpty()}");
+
+            // print the values
+            Console.Write("Array values:  ");
+            foreach (var val in numbers)
+                Console.Write($"{val}   ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            numbers.Clear();
+            Console.WriteLine("Cleared numbers array.");
+            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
         }
     }
 
-    // this class is only built to work with string arrays
-    class MyArrayList
+    // this example class would only work with string arrays:
+    //class ArrayListStrExample
+    //{
+    //    public ArrayListStrExample() 
+    //    {
+    //        StrArrayOnly = new string[5];  // str array with capacity of 5 elements
+    //    }
+    //}
+
+
+    // this class will create an ArrayList from scratch
+    // it can be used with different data types (e.g. string, int, double, bool)
+    // "type safe"; "generic class"; "generic programming"
+    class MyArrayList<T> : IEnumerable  // <T> is for any data type; IEnumerable is for our GetEnumerator() constructor
     {
-        // DATA
-        // prop+tab+tab
-        public int Size { get; private set; }
+        // DATA SECTION
+        // prop+tab+tab shortcut for adding a property
+        public int Size { get; private set; }  // Number of elements in the array
         // public: to access it in main method; "access modifier"
-        // private set: so the value can only be changed in this class; read-only elsewhere 
+        // private set: the value can only be changed in this class; read-only elsewhere 
 
-        public int Capacity { get; private set; }
+        public int Capacity { get; private set; }  // How many elements the array is able to hold
 
-        public string[] Values;  // points to null until a new object is created
+        public T[] Values;  // an array of any data type; Values points to null until a new object is created
 
-        // METHOD(S)
-        public void Add(string newValue)  //  add value to end of array
+
+        // METHOD(S) SECTION
+        public void Add(T newValue)  //  add an element value to end of array
         {
             // check if the array is full
             if(Size == Capacity)
@@ -77,49 +140,48 @@ namespace _2_15_22_classwork
             // put the value at the end of the array
             Values[Size] = newValue;
             
-            // increase the size of the array to account for this new value
+            // increase the count of the array to account for this new element
             Size++;
         }
 
-        public void AddLast(string newValue)  //  add value to end of array; same as Add()
+        public void AddLast(T newValue)  //  add an element value to end of array; same as Add()
         {
             Add(newValue);
         }
 
-        public void AddFirst(string newValue)  //  add value to beginning of array
+        public void AddFirst(T newValue)  // add an element value to beginning of array
         {
-            // shift all current values to the right 
-            // start from the END of the array to do this so you don't override any values
             Insert(newValue, 0);
+            // will shift all current values to the right one spot
+            // will start from the END of the array so you don't override any values
         }
 
-        public void Insert(string newValue, int position)
+        public void Insert(T newValue, int position)  // add an element anywhere in the array
         {
             // check if the array is full
             if(Size == Capacity)
-            {
                 DoubleTheSize();
-            }
 
-            // 
-            if (position > Size)  // this would leave a gap
-                throw new Exception($"You can't leave gaps; position should be at most {Size}");
+            // this situation would leave a gap between elements; elements need to be contiguous
+            if (position > Size) 
+                throw new Exception($"You can't leave gaps between elements; position argument should be at most {Size}.");
 
-            // shift to the right all the elements from position and on; shift them one position to the right
-            for (int i = Size-1; i >= position; i--)  // right to left (Size-1 is the last element in the array)
+            // shift all elements from the position argument to the right one spot
+            // start from the END of the array so you don't override any values
+            for (int i = Size-1; i >= position; i--)  // working from right to left in the array (Count-1 is the last element position in the array)
                 Values[i+1] = Values[i];  // move the value to the right
 
-            // insert newValue at position
+            // insert newValue at position argument
             Values[position] = newValue;
 
-            // increase Size
+            // increase the count of the array to account for this new element
             Size++;
         }
 
         public void DoubleTheSize()  // change to private if you don't want the main method to be able to do this
         {
             // create a new array of double the capacity
-            string[] largerArr = new string[Capacity*2];
+            T[] largerArr = new T[Capacity*2];
 
             // copy all elements from the old array to the new array
             for (int i = 0; i < Size; i++)
@@ -139,33 +201,38 @@ namespace _2_15_22_classwork
             //else
             //    return false;
 
-            return Size == 0;
+            return Size == 0;  // will return true or false
         }
 
         public void Clear()
         {
-            //Size = 0;  // not safe because values are technically still there; just moves size pointer back to beginning of array
+            //Count = 0;  
+            // not safe because values are technically still there and accessible
+            // this just resets the Count variable to 0
 
             // safer:
             Size = 0;
-            Capacity = 4; // or whatever default you want
-            Values = new string[Capacity];  // creates brand new array
+            Capacity = 4; // or whatever default number you want
+            Values = new T[Capacity];  // creates a brand new empty array that Values points to
         }
 
-        // CONSTRUCTOR(S)
+        // so we can use a foreach loop on our class (needs :IEnumerable next to class name)
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Size; i++)
+                yield return Values[i];
+                // yield return: to go through the whole loop and return in each iteration
+
+        }
+   
+
+        // CONSTRUCTOR(S) SECTION
         // when new object is created, constructor is called
         public MyArrayList(int initialCapacity = 4)  // if nothing is passed, 4 will be used; "default parameter"
         {
             Size = 0;
             Capacity = initialCapacity;
-            Values = new string[Capacity];  // Values now pointing to new object
+            Values = new T[Capacity];  // Values now pointing to new object
         }
     }
-
-    // Generic class "type safe"
-    //class MyArrayList<T>
-    //{
-    //    // and where ever string is replaced it with T
-    //}
-
 }
