0633e0f0e1df77c85dccddfab5af20ed5c3953c9
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-02-18 17:57:22 -0800

    renamed Size to Count; added comment at the beginning

--- 2-15-22 classwork/2-15-22 classwork/Program.cs
+++ 2-15-22 classwork/2-15-22 classwork/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections;  // for built-in ArrayList
-using System.Collections.Generic;
+using System.Collections;  // for built-in ArrayList used here
+using System.Collections.Generic;  // for generic programming built-in List<> NOT used here
 
 namespace _2_15_22_classwork
 {
@@ -52,16 +52,16 @@ namespace _2_15_22_classwork
             Console.WriteLine("STRING ARRAY");
             Console.WriteLine($"Array is empty: {myList.IsEmpty()}");
 
-            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");  // .Size and .Capacity properties were created in the MyArrayList class below
+            Console.WriteLine($"Count: {myList.Count}, capacity: {myList.Capacity}");  // .Size and .Capacity properties were created in the MyArrayList class below
 
             myList.Add("Saint");
-            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+            Console.WriteLine($"Count: {myList.Count}, capacity: {myList.Capacity}");
 
             myList.Add("Martin's");
-            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+            Console.WriteLine($"Count: {myList.Count}, capacity: {myList.Capacity}");
 
             myList.Add("University");
-            Console.WriteLine($"Count: {myList.Size}, capacity: {myList.Capacity}");
+            Console.WriteLine($"Count: {myList.Count}, capacity: {myList.Capacity}");
 
 
             Console.WriteLine($"Array is empty: {myList.IsEmpty()}");
@@ -79,15 +79,15 @@ namespace _2_15_22_classwork
             Console.WriteLine($"Array is empty: {numbers.IsEmpty()}");
 
             numbers.Add(30);
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
             numbers.AddFirst(10);
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
             numbers.Insert(20, 1);
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
             numbers.Add(40);
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
             numbers.AddLast(50);
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
 
             Console.WriteLine($"Array is empty: {numbers.IsEmpty()}");
 
@@ -100,7 +100,7 @@ namespace _2_15_22_classwork
 
             numbers.Clear();
             Console.WriteLine("Cleared numbers array.");
-            Console.WriteLine($"Count: {numbers.Size}, capacity: {numbers.Capacity}");
+            Console.WriteLine($"Count: {numbers.Count}, capacity: {numbers.Capacity}");
         }
     }
 
@@ -121,7 +121,7 @@ namespace _2_15_22_classwork
     {
         // DATA SECTION
         // prop+tab+tab shortcut for adding a property
-        public int Size { get; private set; }  // Number of elements in the array
+        public int Count { get; private set; }  // Number of elements in the array
         // public: to access it in main method; "access modifier"
         // private set: the value can only be changed in this class; read-only elsewhere 
 
@@ -134,14 +134,14 @@ namespace _2_15_22_classwork
         public void Add(T newValue)  //  add an element value to end of array
         {
             // check if the array is full
-            if(Size == Capacity)
+            if(Count == Capacity)
                 DoubleTheSize();
 
             // put the value at the end of the array
-            Values[Size] = newValue;
+            Values[Count] = newValue;
             
             // increase the count of the array to account for this new element
-            Size++;
+            Count++;
         }
 
         public void AddLast(T newValue)  //  add an element value to end of array; same as Add()
@@ -159,23 +159,23 @@ namespace _2_15_22_classwork
         public void Insert(T newValue, int position)  // add an element anywhere in the array
         {
             // check if the array is full
-            if(Size == Capacity)
+            if(Count == Capacity)
                 DoubleTheSize();
 
             // this situation would leave a gap between elements; elements need to be contiguous
-            if (position > Size) 
-                throw new Exception($"You can't leave gaps between elements; position argument should be at most {Size}.");
+            if (position > Count) 
+                throw new Exception($"You can't leave gaps between elements; position argument should be at most {Count}.");
 
             // shift all elements from the position argument to the right one spot
             // start from the END of the array so you don't override any values
-            for (int i = Size-1; i >= position; i--)  // working from right to left in the array (Count-1 is the last element position in the array)
+            for (int i = Count-1; i >= position; i--)  // working from right to left in the array (Count-1 is the last element position in the array)
                 Values[i+1] = Values[i];  // move the value to the right
 
             // insert newValue at position argument
             Values[position] = newValue;
 
             // increase the count of the array to account for this new element
-            Size++;
+            Count++;
         }
 
         public void DoubleTheSize()  // change to private if you don't want the main method to be able to do this
@@ -184,7 +184,7 @@ namespace _2_15_22_classwork
             T[] largerArr = new T[Capacity*2];
 
             // copy all elements from the old array to the new array
-            for (int i = 0; i < Size; i++)
+            for (int i = 0; i < Count; i++)
                 largerArr[i] = Values[i];
 
             // redirect Values to point to the new array
@@ -201,7 +201,7 @@ namespace _2_15_22_classwork
             //else
             //    return false;
 
-            return Size == 0;  // will return true or false
+            return Count == 0;  // will return true or false
         }
 
         public void Clear()
@@ -211,7 +211,7 @@ namespace _2_15_22_classwork
             // this just resets the Count variable to 0
 
             // safer:
-            Size = 0;
+            Count = 0;
             Capacity = 4; // or whatever default number you want
             Values = new T[Capacity];  // creates a brand new empty array that Values points to
         }
@@ -219,7 +219,7 @@ namespace _2_15_22_classwork
         // so we can use a foreach loop on our class (needs :IEnumerable next to class name)
         public IEnumerator GetEnumerator()
         {
-            for (int i = 0; i < Size; i++)
+            for (int i = 0; i < Count; i++)
                 yield return Values[i];
                 // yield return: to go through the whole loop and return in each iteration
 
@@ -230,7 +230,7 @@ namespace _2_15_22_classwork
         // when new object is created, constructor is called
         public MyArrayList(int initialCapacity = 4)  // if nothing is passed, 4 will be used; "default parameter"
         {
-            Size = 0;
+            Count = 0;
             Capacity = initialCapacity;
             Values = new T[Capacity];  // Values now pointing to new object
         }
